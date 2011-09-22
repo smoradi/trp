4df9c9edf3a98fa8746b80953e8fe2fa52ab4953
Sid <saeid3@gmail.com>
2011-09-22 10:51:39 +0330

    Generate project with Rails 3.0.10.
    rails _3.0.10_ new trp -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trp::Application.config.secret_token = 'c2fade57de424874db66c0214b82a90eedbd1e2bd22cfbd74e96651c9239aa320614e912052327b60e970135e97a2235c3d3e4951f353514c80a652b6b637b50'
